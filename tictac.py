a4a0d1d5616bdba0dd35ff5584b556dec3061215
coderz11 <31463382+coderz11@users.noreply.github.com>
2021-07-19 15:23:18 +0800

    Error handling
    Expecting users to handle things wrong and telling them what went wrong.

--- tictac.py
+++ tictac.py
@@ -2,12 +2,20 @@ game = [[0, 0, 0],
         [0, 0, 0], 
         [0, 0, 0]]
 
-def game_board(player, row, column):
-    print("   a  b  c")
-    for count, row in enumerate(game):
-        print(count, row)
+def game_board(game_map, player=0, row=0, column=0, justDisplay=False):
+    try:
+        print("   0  1  2")
+        if not justDisplay:
+            game_map[row][column] = player 
+        for count, row in enumerate(game_map):
+            print(count, row)
+        return game_map
+    except IndexError as e:
+        print("Error : Did you input row column as 0 1 or 2?", e)
+    except Exception as e:
+        print("Something went very wrong!", e)
 
-game_board(0, 0, 0)
+game_board(game, justDisplay=True)
+game_board(game_board, player=1, row=3, column=1)
 #game[0][1] = 1
 #game_board()
-
